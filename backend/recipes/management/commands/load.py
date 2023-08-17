9795609a2a421e56900d9f7e1e9e346e39376e12
Denis Vovk <destiny986ya@yandex.ru>
2023-08-17 20:32:10 +0300

    loader add

---
+++ backend/recipes/management/commands/load.py
@@ -0,0 +1,22 @@
+import csv
+from django.core.management import BaseCommand
+from recipes.models import Ingredient
+
+# https://stackoverflow.com/questions/49610125/whats-the-easiest-way-to-import-a-csv-file-into-a-django-model
+# python manage.py load --path ../data/ingredients.csv
+class Command(BaseCommand):
+    help = 'Загружает ингредиенты'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str)
+
+    def handle(self, *args, **kwargs):
+        path = kwargs['path']
+        with open(path, 'rt', encoding='utf-8') as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                Ingredient.objects.create(
+                    name=row[0],
+                    measurement_unit=row[1],
+                )
+        print('Загрузка завершена')
